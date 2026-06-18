4b77a578f0b5b46321077b35a8ac019c54175b9c
215105denise <215105@student.upm.edu.my>
2026-06-18 13:57:40 +0800

    changes

---
+++ My project (2)/Assets/button.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class  button : MonoBehaviour
+{
+
+
+    public void Buttonclip()
+    {
+
+
+        SceneManager.LoadScene("cosmicretrosquid");
+    }
+        
+}
